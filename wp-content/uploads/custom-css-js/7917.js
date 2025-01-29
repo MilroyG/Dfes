a4e3d4d199c255c1e9f504957b4b71bda717fbc4
Milroy <milroygomes98@gmail.com>
2025-01-29 15:03:32 +0530

    Update 7917.js

--- wp-content/uploads/custom-css-js/7917.js
+++ wp-content/uploads/custom-css-js/7917.js
@@ -56,7 +56,7 @@ Saved: Jan 27 2025 | 11:43:23 */
 
   try {
     // Fetch village data
-    const villageResponse = await fetch("http://localhost/wordpress/wp-content/uploads/revenue_villages.json");
+    const villageResponse = await fetch("C:\xampp\htdocs\dfes\dfes\wp-content\uploads");
     const villageData = await villageResponse.json();
 
     // Create a map of village coordinates
